1a6d5c13e031636202cdb0b2a8df49c15df5a6b2
skiddieahn <skd@yonsei.ac.kr>
2025-01-08 16:25:36 +0900

    edit files

--- utils.py
+++ utils.py
@@ -139,13 +139,6 @@ def load_names_paths(cfg):
     return video_names, video_paths
 
 
-def ovad_load_names_paths(cfg):
-    video_names = os.listdir(cfg.test_data_path)
-    video_names.sort()
-    video_paths = [os.path.join(cfg.test_data_path, name) for name in video_names]
-    return video_names, video_paths
-
-
 def load_keyword_list(cfg):
     test_data_name = cfg.type
     if cfg.multiple:
